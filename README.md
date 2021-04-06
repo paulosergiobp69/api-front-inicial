653d91f651ad6dc75f6e57c9477accbede67c5b5
paulosergiobp69 <paulosergiobp@gmail.com>
2021-04-06 18:33:32 -0300

    API-FRONT INICIAL

--- README.md
+++ README.md
@@ -22,3 +22,43 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+projeto desenvolvido com a ajude de:
+
+--> configuracao do vscode e ambiente:
+https://www.youtube.com/watch?v=S-b-reTfJrU&list=PLWd_VnthxxLdISUq_V8CXUer07PmQ7S6P
+
+
+--> VueJS - Consumindo API Rest
+https://www.youtube.com/watch?v=wwq94z8mssY&list=PLWd_VnthxxLeRdaga093nbR64dALpbPwI&index=1
+
+1. baixar e instalar: node, comander, vuejs 
+node ----> https://nodejs.org
+
+comander --> https://cmder.net/
+
+vuejs ---> https://cli.vuejs.org
+
+
+1.1. instalando vuejs
+		npm install -g @vue/cli
+		
+		
+2. Criar projeto VUEJS
+	vue create api-front
+	
+3. acessar projeto
+
+> cd api-front
+
+> npm run serve
+
+3.1. adicionar a extenção vue tools no browser
+3.2. adicionar a extenção [vetur - pe wui] no vscode 
+
+
+4. copia plugin materialize
+
+5. adicionar axios
+npm install axios --save 
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,175 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">Ordem de Compra -  id:{{h100.H100_Id}}</a>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <form @submit.prevent="salvar">
+
+         <section v-if="errored">
+              <b>Por favor, corrija o(s) seguinte(s) erro(s):</b>
+              <ul>
+                <li v-for="(erro,index) of errors" :key="index">
+                  Campo: <b>{{ erro }}</b> 
+                </li>
+              </ul>       
+          </section>
+
+          <label>Produto</label>
+          <input type="text" placeholder="Produto" v-model="h100.H100_D009_Id">
+          <label>Item O.C.</label>
+          <input type="text" placeholder="Item O.C." v-model="h100.H100_T012_Id">
+          <label>Quantidade</label>
+          <input type="text" placeholder="QTD" v-model="h100.H100_Quantidade">
+          <label>Valor</label>
+          <input type="text" placeholder="Valor" v-model="h100.H100_Valor_Unitario">
+          <label>Usuario</label>
+          <input type="text" placeholder="Usuario" v-model="h100.H100_C007_Id">
+          <label>Status</label>
+          <input type="text" placeholder="Status" v-model="h100.H100_Status">
+
+          <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+
+      </form>
+
+      <table>
+
+        <thead>
+
+          <tr>
+            <th>PRODUTO</th>
+            <th>ITEM OC</th>
+            <th>QTD</th>
+            <th>VALOR</th>
+            <th>USUARIO</th>
+            <th>STATUS</th>
+            <th>OPÇÕES</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+            <tr v-for="h100 of h100s.data" :key="h100.H100_Id">
+
+            <td>{{h100.H100_D009_Id}}</td>
+            <td>{{h100.H100_T012_Id}}</td>
+            <td>{{h100.H100_Quantidade}}</td>
+            <td>{{h100.H100_Valor_Unitario}}</td>
+            <td>{{h100.H100_C007_Id}}</td>
+            <td>{{h100.H100_Status}}</td>
+            <td>
+              <button @click="editar(h100)" class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
+              <button @click="excluir(h100.H100_Id)" class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+            </td>
+
+          </tr>
+
+        </tbody>
+      
+      </table>
+
+    </div>
+
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Cabecalho from './service/h100'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+  data(){
+      return {
+          errors: [],
+          errored: false,
+          loading: true,
+          message_error: 'Pedimos desculpas, não estamos conseguindo recuperar as informações no momento. Por favor, tente novamente mais tarde.',
+          h100: {
+            H100_Id:'',
+            H100_D009_Id:'',
+            H100_T012_Id:'',
+            H100_Quantidade:'',
+            H100_Valor_Unitario:'',
+            H100_C007_Id:'',
+            H100_Status:''
+          },
+          h100s: []
+      }
+  },
+  mounted(){
+    this.listar()
+  },
+
+  methods:{
+
+    listar(){
+      Cabecalho.listar().then(resposta => {
+          this.h100s = resposta.data
+      })
+    },
+
+    salvar(){
+      if(!this.h100.H100_Id){
+          Cabecalho.salvar(this.h100).then(resposta => {
+            this.h100s  = resposta.data
+            alert('Salvo com sucesso!')
+            this.h100 = {}
+            this.errored = false
+            this.errors = []
+            this.listar()
+          })
+          .catch(error => {
+            this.errors = error.response.data.errors
+            this.errored = true
+          })
+      }else{
+          Cabecalho.atualizar(this.h100).then(resposta => {
+            alert('Atualizado com sucesso!')
+            this.h100s  = resposta.data.data
+            this.h100 = {}
+            this.errored = false
+            this.errors = []
+            this.listar()
+          
+          })
+          .catch(error => {
+            this.errors = error.response.data.errors
+            this.errored = true
+          })
+      }
+
+    },
+
+    editar(h100){
+      this.h100 = h100
+    },
+
+    excluir(h100){
+      if(confirm('Deseja excluir a O.C.?')){
+          Cabecalho.apagar(h100).then(resposta => {
+            this.h100s  = resposta.data
+            this.listar(),
+            this.errors = []
+          }).catch(e => {
+            this.errors = e.response.data.errors
+          })
+      }
+    }
+
+
   }
+  
+
+
 }
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
